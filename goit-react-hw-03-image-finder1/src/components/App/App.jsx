fc5b0b4a43f4dc2f0951a4091951b2145564ed59
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-12-01 12:49:56 +0100

    init project

---
+++ goit-react-hw-03-image-finder1/src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+// import { nanoid } from "nanoid";
+// import {} from "components";
+
+class App extends Component {
+  render() {
+    return <p>HEJ</p>;
+  }
+}
+
+export default App;
